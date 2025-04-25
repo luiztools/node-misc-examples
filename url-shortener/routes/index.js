d70633415215e752a36ce7e9ead004a73c74a8f0
LuizTools <luizfduartejr@gmail.com>
2025-04-25 20:17:58 -0300

    update 2025 (packages and syntax)

--- url-shortener/routes/index.js
+++ url-shortener/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const Link = require('../models/link');
 
 router.get('/:code/stats', async (req, res, next) => {
