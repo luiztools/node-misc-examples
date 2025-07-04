0a807a5ee240644c939a5569117e60d717106d31
LuizTools <luizfduartejr@gmail.com>
2025-07-04 16:26:31 -0300

    jwt with rsa example

---
+++ jwt-rsa/README.md
@@ -0,0 +1,22 @@
+# JSON Web Token - RSA Example
+Node.js webapi example with JWT auth + RSA cryptography.
+
+## How to Run
+
+1. git clone
+2. cd jwt-rsa
+3. npm install
+4. create .env file
+5. fill .env variables as .env.example
+6. create your keys files and replace the examples
+7. npm start
+
+## More
+
+Para o tutorial completo, acesse: https://www.luiztools.com.br/post/autenticacao-json-web-token-jwt-em-node-js-2/
+
+Para cursos, acesse: https://www.luiztools.com.br/meus-cursos
+
+Para um livro completo, acesse: https://www.luiztools.com.br/livro-node-ii
+
+Me siga nas redes sociais e Telegram: https://about.me/luiztools
--- jwt-rsa/index.js
+++ jwt-rsa/index.js
@@ -1,5 +1,6 @@
 //index.js
 require("dotenv-safe").config();
+const fs = require("fs");
 const jwt = require("jsonwebtoken");
 
 const express = require("express");
@@ -7,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.json({ message: "Tudo ok por aqui!" });
 })
 
@@ -19,12 +20,13 @@ app.get("/clientes", verifyJWT, (req, res) => {
 function verifyJWT(req, res, next) {
   let token = req.headers["authorization"];
   if (!token) return res.status(401).json({ message: "No token provided." });
-  
+
   token = req.headers["authorization"].replace("Bearer ", "");
   if (blacklist[token]) return res.status(403).json({ message: "Invalid token." });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const publicKey = fs.readFileSync("./public.key", "utf8");
+    const decoded = jwt.verify(token, publicKey, { algorithm: ["RS256"] });
     if (!decoded) return res.status(403).json({ message: "Invalid token." });
 
     res.locals.token = decoded;
@@ -39,9 +41,13 @@ app.post("/login", (req, res, next) => {
   if (req.body.user === "luiz" && req.body.password === "123") {
     //auth ok
     const userId = 1; //esse id viria do banco de dados
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: parseInt(process.env.JWT_EXPIRES)
+
+    const privateKey = fs.readFileSync("./private.key", "utf8");
+    const token = jwt.sign({ userId }, privateKey, {
+      expiresIn: parseInt(process.env.JWT_EXPIRES),
+      algorithm: "RS256"
     });
+
     return res.json({ token: token });
   }
 
